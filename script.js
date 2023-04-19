2ca0871f8fffca437e60924b24b003091ea842cb
Napster <rome.1205h@gmail.com>
2023-04-19 18:36:09 -0500

    final update

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 const addBtn = document.getElementById("add");
 
 const notes = JSON.parse(localStorage.getItem("notes"));
-if(notes) {
-   notes.forEach((note) => {
-      addNewNote(note)
-   })
-}
 
-addBtn.addEventListener("click", () => {
-   addNewNote();
-})
 
 const addNewNote = ( text = "" ) => {
    const note = document.createElement("div");
@@ -45,5 +37,32 @@ const addNewNote = ( text = "" ) => {
       updateLS()
    })
 
+   textArea.addEventListener("input", (e) => {
+      const { value } = e.target;
+      main.innerHTML = marked(value);
+      updateLS();
+   })
+
    document.body.appendChild(note)
 }
+
+if(notes) {
+   notes.forEach((note) => {
+      addNewNote(note)
+   })
+}
+
+addBtn.addEventListener("click", () => {
+   addNewNote();
+})
+
+const updateLS = () => {
+   const notesText = document.querySelectorAll("textarea");
+
+   const notes = [];
+   notesText.forEach((note) => {
+      notes.push(note.value);
+   })
+   localStorage.setItem("notes", JSON.stringify(notes));
+}
+
